608f1e634257c5b694147d174ced15c6a6cd4071
Danubio Simione <ndanubio@gmail.com>
2021-01-22 15:17:34 +0200

    msg

---
+++ src/aula03/Aula03.java
@@ -0,0 +1,19 @@
+package aula03;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nomAluno;
+		nomAluno = "Danubio";
+		int naluno;
+		naluno = 30;
+		long codigo;
+		codigo= 5000752;
+		double notaDiscipl;
+		notaDiscipl= 10.10;
+				
+		
+	}
+
+}
---
+++ src/aula03/Tpc2.java
@@ -0,0 +1,22 @@
+package aula03;
+
+public class Tpc2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String OutrosNomes, apelido, instrutor, NomedoCurso, local, intrutor;
+		
+		OutrosNomes = "Danubio Arbanosse";
+		apelido= "Simione";
+		instrutor ="Momade abdul";
+		NomedoCurso = "Logica de programacao";
+		local ="Centro de Informatica da UEM";
+		System.out.println("---------------------Certificado de Particiapacao ---------------");
+		System.out.println("O estudante "+ OutrosNomes + " " + apelido + " frequentou com sucesso ");
+		System.out.println("O curso de  " +NomedoCurso + " no Centro de Informatica da UEM" );
+		System.out.println("Novo brincadeira");
+	}
+
+}
